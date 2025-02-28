001bfd90c7a91928f4415f635e49e7ea699e917f
DanPasqua-CF <dan.pasqua.cf@gmail.com>
2025-02-27 22:42:32 -0500

    Add three objects to pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,30 @@
-alert('Hello world!');
+let pokemonObject2 = {
+  name: "Tentacool",
+  height: 2,
+  types: [
+    "water",
+    "poison"
+  ]
+};
 
-const favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonObject3Types = ["normal", "fairy"];
+
+let pokemonList = [
+  {
+    name: "Bulbasaur",
+    height: 7,
+    types: ["grass", "poison"]
+  },
+  {
+    name: pokemonObject2.name,
+    height: pokemonObject2.height,
+    types: pokemonObject2.types
+  },
+  {
+    name: "Igglybuff",
+    height: 1,
+    types: pokemonObject3Types
+  }
+];
+
+console.log(pokemonList);
